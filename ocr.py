85df8e7e4b893998bb0062a5fb6e68d7c76ae1fa
sihem <fs_berrichi@esi.dz>
2020-02-02 12:06:28 +0100

    Procfile added

--- ocr.py
+++ ocr.py
@@ -8,7 +8,7 @@ def get_code(path):
     data = []
     originalImage = cv2.imread(path)
 
-    slicedImage = originalImage[140:400, 500:1500]
+    slicedImage = originalImage[140:400, 800:1500]
     gray = cv2.cvtColor(slicedImage, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
@@ -17,14 +17,16 @@ def get_code(path):
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                 cv2.THRESH_BINARY, 11, 5)
     img = cv2.erode(img, kernel, iterations=1)
-    res2 = pytesseract.image_to_string(img)
-    data.append(res2)
+    cv2.imshow("image",img)
+
+    res = pytesseract.image_to_string(img)
+    data.append(res)
     return data
 
 
 
 
 res=get_code('ocr.jpg')
-print("L'erreur est :", res[0])
+print(" L'erreur:", res[0])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
